75ce4185d00b8d464dcbeec78cac41c5da0884c0
Gonior <43252085+Gonior@users.noreply.github.com>
2019-09-23 18:16:15 +0700

    proses push ke github

---
+++ src/pbo11k/pkg10118901/latihan2/variabel/PBO11K10118901Latihan2Variabel.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118901.latihan2.variabel;
+
+/**
+ *
+ * @author
+ * NAMA         : Dedi Cahya
+ * KELAS        : IF-11-K
+ * NIM          : 10118901
+ * DESKRIPSI    : Program untuk deklarasi variabel dan mengaksesnya
+ */
+public class PBO11K10118901Latihan2Variabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai      : " + nilaiInt);
+        System.out.println("Isi variabel PHI        : "+PHI);
+        System.out.println("Isi variabel logika     : "+nilaiLogika);
+        System.out.println("Isi variabel karakter   : "+nilaiKarakter);
+                
+    }
+    
+}
